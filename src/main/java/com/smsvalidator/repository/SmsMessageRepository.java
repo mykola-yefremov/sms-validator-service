a6148d9ff1bc9f6360775165ddb50d27f7cc489f
Mykola Yefremov <efremovnikolaus@gmail.com>
2025-07-03 20:00:54 +0200

    feat: add build

--- src/main/java/com/smsvalidator/repository/SmsMessageRepository.java
+++ src/main/java/com/smsvalidator/repository/SmsMessageRepository.java
@@ -1,5 +1,6 @@
 package com.smsvalidator.repository;
 
+import com.smsvalidator.entity.SmsMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
