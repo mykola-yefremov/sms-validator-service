a3e99a7d60a278a9f1267a80e78c9264c57de530
Mykola Yefremov <efremovnikolaus@gmail.com>
2025-07-03 11:13:22 +0200

    feat: new dependencies & fix

---
+++ src/main/java/com/smsvalidator/service/SmsMessageService.java
@@ -0,0 +1,9 @@
+package com.smsvalidator.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class SmsMessageService {
+}
